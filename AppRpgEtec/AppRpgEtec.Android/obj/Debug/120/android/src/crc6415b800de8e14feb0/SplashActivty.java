2a5a78460826b27a610c7d64f76408540929d05c
Gabrielly-Passos <passosgabrielly03@gmail.com>
2022-08-16 22:11:18 -0300

    [CREATE]

---
+++ AppRpgEtec/AppRpgEtec.Android/obj/Debug/120/android/src/crc6415b800de8e14feb0/SplashActivty.java
@@ -0,0 +1,65 @@
+package crc6415b800de8e14feb0;
+
+
+public class SplashActivty
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;Landroid/os/PersistableBundle;)V:GetOnCreate_Landroid_os_Bundle_Landroid_os_PersistableBundle_Handler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"n_onBackPressed:()V:GetOnBackPressedHandler\n" +
+			"";
+		mono.android.Runtime.register ("AppRpgEtec.Droid.SplashActivty, AppRpgEtec.Android", SplashActivty.class, __md_methods);
+	}
+
+
+	public SplashActivty ()
+	{
+		super ();
+		if (getClass () == SplashActivty.class)
+			mono.android.TypeManager.Activate ("AppRpgEtec.Droid.SplashActivty, AppRpgEtec.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0, android.os.PersistableBundle p1)
+	{
+		n_onCreate (p0, p1);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0, android.os.PersistableBundle p1);
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+
+	public void onBackPressed ()
+	{
+		n_onBackPressed ();
+	}
+
+	private native void n_onBackPressed ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
