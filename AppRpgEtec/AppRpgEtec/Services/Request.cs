c0f97346c65c2fe0573ca7a0a9571f996154e714
Gabrielly-Passos <passosgabrielly03@gmail.com>
2022-08-16 22:41:09 -0300

    [UPDATE] Request

---
+++ AppRpgEtec/AppRpgEtec/Services/Request.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace AppRpgEtec.Services
+{
+    public class Request
+    {
+        public async Task<string> PostReturnStringAsync(TResult)(string uri, TResult data)
+        {
+            HttpClient httpClient = new HttpClient();
+
+        var content = new StringContent/ JsonConvert.SerializeObject(data);
+        content Headers ContentType = new MediaTypeHeaderValue("application/json");
+
+        }
+    }
+
