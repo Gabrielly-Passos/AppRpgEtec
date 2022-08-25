af6f6d2d06626bce553871f6b63e8987fba3114e
Gabrielly-Passos <passosgabrielly03@gmail.com>
2022-08-24 21:30:47 -0300

    [UPDATE] BaseView

--- AppRpgEtec/AppRpgEtec/Services/Usuarios/UsuarioService.cs
+++ AppRpgEtec/AppRpgEtec/Services/Usuarios/UsuarioService.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Models;
+
 
 namespace AppRpgEtec.Services.Usuarios
 {
-    internal class UsuarioService
+    public class UsuarioService : Request
     {
+        private readonly Request _request;
+        private const string ApiUrlBase = "Minha API";
+
+        public UsuarioService()
+        {
+            _request = new Request();
+        }
+
+        public async Task<Usuario> PostLoginUsuarioAsync(Usuario u)
+        {
+            //Autenticar: Rota para método na API que verifica usuário e senha 
+            string urlComplementar = "/Autenticar";
+            u.Token = await _request.PostReturnStringAsync(ApiUrlBase + urlComplementar, u);
+            return u;
+        }
+
+
     }
 }
