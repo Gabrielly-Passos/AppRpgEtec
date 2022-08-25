7b69985ba8f5d4a9d4f5532b487a58864b07c1b6
Gabrielly-Passos <passosgabrielly03@gmail.com>
2022-08-24 21:07:17 -0300

    [UPDATE] Usuarios

--- AppRpgEtec/AppRpgEtec/Services/Request.cs
+++ AppRpgEtec/AppRpgEtec/Services/Request.cs
@@ -4,18 +4,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace AppRpgEtec.Services
 {
     public class Request
     {
-        public async Task<string> PostReturnStringAsync(TResult)(string uri, TResult data)
+        public async Task<string> PostReturnStringAsync<TResult>(string uri, TResult data)
         {
             HttpClient httpClient = new HttpClient();
-
-        var content = new StringContent/ JsonConvert.SerializeObject(data);
-        content Headers ContentType = new MediaTypeHeaderValue("application/json");
-
+            var content = new StringContent(JsonConvert.SerializeObject(data));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await httpClient.PostAsync(uri, content);
+            string serialized = await response.Content.ReadAsStringAsync();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return serialized;
+            else
+                return string.Empty;
         }
+      }
     }
 
