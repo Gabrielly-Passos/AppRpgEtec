f11027310bf151733004f89181d8dcac4ea0a3f1
gabrielly-passos <passosgabrielly03@gmail.com>
2022-08-30 21:46:19 -0300

    [UPDATE] Criacao Login

--- AppRpgEtec/AppRpgEtec/ViewModels/Usuarios/UsuarioViewModel.cs
+++ AppRpgEtec/AppRpgEtec/ViewModels/Usuarios/UsuarioViewModel.cs
@@ -15,26 +15,94 @@ namespace AppRpgEtec.ViewModels.Usuarios
     public class UsuarioViewModel : BaseViewModel
     {
         private UsuarioService uService;
-        private Usuario Usuario; 
+        private Usuario Usuario;
 
-        public ICommand EntrarCommand { get; set;  }
+        public ICommand EntrarCommand { get; set; }
 
-    }
 
-    public async Task ConsultarUsuario()
-    {
-        try
+        public UsuarioViewModel()
         {
-            //Próxima codificação
-
-            Usuario u
+            this.Usuario = new Usuario();
+            uService = new UsuarioService();
+            RegistrarCommands();
 
         }
-        catch (Exception ex)
+        public void RegistrarCommands()
         {
-            await Application.Current.MainPage
-                .DisplayAlert("Informação", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            EntrarCommand = new Command(async () => { await ConsultarUsuario(); });
         }
-    }
 
+        public async Task ConsultarUsuario()
+        {
+            try
+            {
+                //Próxima codificação
+
+                Usuario u = null;
+                u = await uService.PostLoginUsuarioAsync(Usuario);
+
+                if (!string.IsNullOrEmpty(u.Token))
+                {
+                    Application.Current.Properties["UsuarioId"] = u.Id;
+                    Application.Current.Properties["UsuarioUsername"] = u.Username;
+                    Application.Current.Properties["UsuarioPerfil"] = u.Perfil;
+                    Application.Current.Properties["UsuarioToken"] = u.Token;
+
+                    string mensagem = string.Format("Bem-Vindo (0)", u.Username);
+                    await Application
+                        .Current.MainPage.DisplayAlert("Informação", mensagem, "Ok");
+                    Application.Current.MainPage = new AppRpgEtec.MainPage();
+
+
+                }
+
+                else
+                {
+                    await Application.Current.MainPage
+                        .DisplayAlert("Informação", "Dados Incorretos :(", "Ok");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Informação", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+        #region ViewProperties
+
+        public string Login
+        { get { return this.Usuario.Username; }
+
+            set
+            {
+                this.Usuario.Username = value;
+                OnPropertyChanged();
+            }
+
+
+        }
+
+        public string Senha
+        {
+          get { return this.Usuario.PasswordString; }
+
+            set
+            {
+                this.Usuario.PasswordString = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+
+
+
+
+
+
+
+    }
 }
