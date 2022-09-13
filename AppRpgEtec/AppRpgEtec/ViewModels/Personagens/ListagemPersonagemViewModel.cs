aa9e9dd340252d3355669e419f54ff8bc1ddc383
Gabrielly-passos <passosgabrielly03@gmail.com>
2022-09-13 20:37:16 -0300

    [UPDATE] ListagemPersonagemViewModel Criacao

---
+++ AppRpgEtec/AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -0,0 +1,24 @@
+﻿using AppRpgEtec.Models;
+using AppRpgEtec.Services.Personagens;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class ListagemPersonagemViewModel  : BaseViewModel
+    {
+        private PersonagemService pService;
+
+        private ObservableCollection<Personagem> Personagens { get; set; }
+
+        public ListagemPersonagemViewModel()
+        {
+            string token = Application.Current.Properties["UsuarioToken"].ToString();
+            pService = new PersonagemService(token);
+            Personagens = new ObservableCollection<Personagem>();
+        }
+    }
+}
