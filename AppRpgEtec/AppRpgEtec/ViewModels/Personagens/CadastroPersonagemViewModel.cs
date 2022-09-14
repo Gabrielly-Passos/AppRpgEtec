3c64cf3d58beb744a388f173cce71dc8f8c78e0e
Gabrielly-passos <passosgabrielly03@gmail.com>
2022-09-13 22:11:19 -0300

    [UPDATE] Criacao atributos

---
+++ AppRpgEtec/AppRpgEtec/ViewModels/Personagens/CadastroPersonagemViewModel.cs
@@ -0,0 +1,121 @@
+﻿using AppRpgEtec.Services.Personagens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+using AppRpgEtec.Models;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Application = Xamarin.Forms.Application;
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class CadastroPersonagemViewModel : BaseViewModel
+    {
+        private PersonagemService pService;
+
+        public CadastroPersonagemViewModel()
+        {
+            string token = Application.Current.Properties["UsuarioToken"].ToString();
+            pService = new PersonagemService(token);
+        }
+
+        private int id;
+        private string nome;
+        private int pontosVida;
+        private int forca;
+        private int defesa;
+        private int inteligencia;
+        private int disputas;
+        private int vitorias;
+        private int derrotas;
+
+        public int Id
+        {
+            get => id;
+            set
+            {
+                id = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Nome
+        {
+            get => nome;
+            set
+            { nome = value;
+                OnPropertyChanged();
+            }
+        }
+        public int PontosVida
+        {
+            get => pontosVida;
+            set
+            {
+                pontosVida = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Forca
+        {
+            get => forca;
+            set
+            {
+                forca = value;
+                OnPropertyChanged();
+
+            }
+        }
+        public int Defesa
+        {
+            get => defesa;
+            set
+            {
+                defesa = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Inteligencia
+        {
+            get => inteligencia;
+            set
+            {
+                inteligencia = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Disputas
+        {
+            get => disputas;
+            set
+            {
+                disputas = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Vitorias
+        {
+            get => vitorias;
+            set
+            {
+                vitorias = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Derrotas
+        {
+            get => derrotas;
+            set
+            {
+                derrotas = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+
+
+    }
+}
